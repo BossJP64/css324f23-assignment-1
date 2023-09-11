238bfff1d9ba1f4a36ae27d91ed0d2896f17696a
BossJP64 <144695608+BossJP64@users.noreply.github.com>
2023-09-11 20:22:42 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -1,9 +1,35 @@
 def initial_state():
-    return (0, 0, 0)
+    return (8, 0, 0)
 
 def is_goal(s):
-    return True
+    return s[0] == 4 and s[1] == 4
 
 def successors(s):
     x, y, z = s
-    return []
+    actions = []
+
+    if x > 0 and y < 5:
+        pour_amount = min(x, 5 - y)
+        actions.append(("8 to 5", (x - pour_amount, y + pour_amount, z)))
+
+    if x > 0 and z < 3:
+        pour_amount = min(x, 3 - z)
+        actions.append(("8 to 3", (x - pour_amount, y, z + pour_amount)))
+
+    if y > 0 and x < 8:
+        pour_amount = min(y, 8 - x)
+        actions.append(("5 to 8", (x + pour_amount, y - pour_amount, z)))
+
+    if y > 0 and z < 3:
+        pour_amount = min(y, 3 - z)
+        actions.append(("5 to 3", (x, y - pour_amount, z + pour_amount)))
+
+    if z > 0 and x < 8:
+        pour_amount = min(z, 8 - x)
+        actions.append(("3 to 8", (x + pour_amount, y, z - pour_amount)))
+
+    if z > 0 and y < 5:
+        pour_amount = min(z, 5 - y)
+        actions.append(("3 to 5", (x, y + pour_amount, z - pour_amount)))
+
+    return actions
